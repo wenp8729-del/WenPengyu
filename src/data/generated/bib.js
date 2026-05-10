0af5594139289a07955237241d8e34b9956653b3
WENP8729-del <wenp8729@gmail.com>
2026-05-10 14:03:25 +0100

    first commit

---
+++ src/data/generated/bib.js
@@ -0,0 +1,101 @@
+﻿define({ entries : {
+    "bardomiano2022limits": {
+        "author": "Bardomiano Mart{\\'\\i}nez, C{\\'e}sar",
+        "doi": "10.1017/S0960129525100248",
+        "journal": "Mathematical Structures in Computer Science",
+        "keywords": "limits, colimits, STT",
+        "title": "Limits and colimits in synthetic \u221e-categories",
+        "type": "article",
+        "volume": "35,",
+        "year": "2022,"
+    },
+    "buchholtz2018higher": {
+        "author": "Buchholtz, Ulrik",
+        "booktitle": "Synthese Library",
+        "doi": "10.1007/978-3-030-15655-8_7",
+        "keywords": "HoTT, higher structures",
+        "title": "Higher Structures in Homotopy Type Theory",
+        "type": "incollection",
+        "year": "2018,"
+    },
+    "buchholtz2023synthetic": {
+        "author": "Buchholtz, Ulrik and Weinberger, Jonathan",
+        "doi": "10.21136/hs.2023.04",
+        "journal": "Higher Structures",
+        "keywords": "fibrations, STT",
+        "number": "1,",
+        "pages": "74-165",
+        "title": "Synthetic fibered (\u221e,1)-category theory",
+        "type": "article",
+        "volume": "7,",
+        "year": "2023,"
+    },
+    "cohen2015cubical": {
+        "author": "Cohen, Cyril and Coquand, Thierry and Huber, Simon and M\u00f6rtberg, Anders",
+        "booktitle": "Types for Proofs and Programs (TYPES 2015)",
+        "doi": "10.4230/LIPIcs.TYPES.2015.5",
+        "keywords": "cubical, HoTT, extension types",
+        "title": "Cubical Type Theory: A Constructive Interpretation of the Univalence Axiom",
+        "type": "inproceedings",
+        "year": "2015,"
+    },
+    "kudasov2023formalizing": {
+        "author": "Kudasov, Nikolai and Riehl, Emily and Weinberger, Jonathan",
+        "booktitle": "Proceedings of the 13th ACM SIGPLAN International Conference on Certified Programs and Proofs (CPP 2024)",
+        "doi": "10.1145/3636501.3636945",
+        "keywords": "Rzk, formalization, Yoneda",
+        "title": "Formalizing the \u221e-Categorical Yoneda Lemma",
+        "type": "inproceedings",
+        "year": "2023,"
+    },
+    "riehl2015fibrations": {
+        "author": "Riehl, Emily and Verity, Dominic",
+        "doi": "10.48550/arXiv.1506.05500",
+        "journal": "arXiv:1506.05500",
+        "keywords": "\u221e-cosmos, fibrations",
+        "title": "Fibrations and Yoneda's lemma in an \u221e-cosmos",
+        "type": "article",
+        "year": "2015,"
+    },
+    "riehl2017type": {
+        "author": "Riehl, Emily and Shulman, Michael",
+        "doi": "10.21136/hs.2017.06",
+        "journal": "Higher Structures",
+        "keywords": "STT, synthetic, \u221e-categories",
+        "number": "1,",
+        "pages": "116-193",
+        "title": "A type theory for synthetic \u221e-categories",
+        "type": "article",
+        "volume": "1,",
+        "year": "2017,"
+    },
+    "riehl2022elements": {
+        "author": "Riehl, Emily and Verity, Dominic",
+        "doi": "10.1017/9781108936880",
+        "keywords": "textbook",
+        "publisher": "Cambridge University Press",
+        "title": "Elements of \u221e-Category Theory",
+        "type": "book",
+        "year": "2022,"
+    },
+    "riehl2023teach": {
+        "author": "Riehl, Emily",
+        "doi": "10.1090/noti2692",
+        "journal": "Notices of the American Mathematical Society",
+        "keywords": "expository",
+        "title": "Could \u221e-Category Theory Be Taught to Undergraduates?",
+        "type": "article",
+        "year": "2023,"
+    },
+    "riehl2024topos": {
+        "author": "Riehl, Emily",
+        "doi": "10.1112/blms.12991",
+        "journal": "Bulletin of the London Mathematical Society",
+        "keywords": "semantics, HoTT",
+        "pages": "461-517",
+        "title": "On the \u221e-topos semantics of homotopy type theory",
+        "type": "article",
+        "volume": "56,",
+        "year": "2024,"
+    }
+}});
